d1eb4089ab0c52b49b9b9cb9404990093c70f14a
Philip <philipstipi@gmx.at>
2026-09-07 18:17:05 +0200

    Bugfixing + Kommentare + tests

--- energy-user/src/main/java/at/fhtw/disys/energycommunities/user/EnergyUserApp.java
+++ energy-user/src/main/java/at/fhtw/disys/energycommunities/user/EnergyUserApp.java
@@ -30,6 +30,11 @@ public class EnergyUserApp {
             Channel channel = connection.createChannel()) {
 
             channel.exchangeDeclare(RabbitMQConfig.EXCHANGE_NAME, "direct", true);
+            // queueDeclare(name, durable, exclusive, autoDelete, arguments):
+            // durable=true    -> übersteht RabbitMQ-Neustart (nicht: Java-Prozess-Shutdown)
+            // exclusive=false -> mehrere Connections/Services dürfen diese Queue nutzen
+            // autoDelete=false -> Queue bleibt bestehen, auch ohne aktiven Consumer
+            // arguments=null  -> keine Extra-Optionen (z.B. TTL, Dead-Letter-Exchange) nötig
             channel.queueDeclare(RabbitMQConfig.QUEUE_ENERGY, true, false, false, null);
             channel.queueBind(RabbitMQConfig.QUEUE_ENERGY, RabbitMQConfig.EXCHANGE_NAME, RabbitMQConfig.ROUTING_KEY_ENERGY);
 
@@ -39,14 +44,14 @@ public class EnergyUserApp {
                 int hour = LocalDateTime.now().getHour();
                 double base;
 
-                if (6 <= hour && hour <= 9) {
+                if (6 <= hour && hour <= 9) {  // 6 - 9 Uhr morgens PEAK
                     base = 0.010;
-                } else if (17 <= hour && hour <= 21) {
+                } else if (17 <= hour && hour <= 21) {  // 17-21 Uhr PEAK
                     base = 0.0125;
-                } else if (22 <= hour|| hour <= 5) {
+                } else if (22 <= hour|| hour <= 5) { // Nacht -> am wenigsten
                     base = 0.002;
                 } else {
-                    base = 0.0055;
+                    base = 0.0055;   // sonst einfach stani Wert
                 }
 
                 double jitter = 0.8 + Math.random() * 0.4;
